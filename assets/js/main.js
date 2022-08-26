65c69f393209ec89bf9b95679a6cd4fda4af8d9f
itfeelsharsh <itfeelsharsh@gmail.com>
2022-08-26 10:06:17 +0530

    Special Image Protector in /ourteam so body can download the pictures *hopefully

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,72 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+$(document).ready(function() {
+  NoClick(); NoTouch(); init()
+  });
+  
+  function NoClick() //turns off right clicking of mouse on desktops for any img with stylesheet class named 'protected'
+  {
+  
+  $('body').on('contextmenu', '.protected', function(e){ return false; }); 
+  }
+  
+  function NoTouch() { // turns off touch events for mobile browsers
+    $("body").on("touchmove", false); 
+    $('body').on('MozTouchMove', '.protected', function(e){e.preventDefault();});   // mozilla (firefox) specific
+  }
+  
+  //android specific to turn off context menu on long touch events
+  function absorbEvent_(event) {
+        var e = event || window.event;
+        e.preventDefault && e.preventDefault();
+        e.stopPropagation && e.stopPropagation();
+        e.cancelBubble = true;
+        e.returnValue = false;
+        return false;
+      }
+      
+      function preventLongPressMenu(node) {
+        node.ontouchstart = absorbEvent_;
+        node.ontouchmove = absorbEvent_;
+        node.ontouchend = absorbEvent_;
+        node.ontouchcancel = absorbEvent_;
+      }
+      
+      function init() {
+        preventLongPressMenu(document.getElementByClassName('protected'));
+      }
+
+      document.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+        console.log('Right Click is Disabled');    
+        }, false);
+    
+        
+    
+        function absorbEvent_(event) {
+      var e = event || window.event;
+      e.preventDefault && e.preventDefault();
+      e.stopPropagation && e.stopPropagation();
+      e.cancelBubble = true;
+      e.returnValue = false;
+      return false;
+    }
+    
+    function preventLongPressMenu(node) {
+      node.ontouchstart = absorbEvent_;
+      node.ontouchmove = absorbEvent_;
+      node.ontouchend = absorbEvent_;
+      node.ontouchcancel = absorbEvent_;
+    }
+    
+    function init() {
+      preventLongPressMenu(document.getElementById('theimage'));
+    }    
+
+    
+
+
 !(function($) {
   "use strict";
 
